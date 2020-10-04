0a1e129696fe2fb447ff4bcae7557af278d2c583
Phill Serrazina <fserrazina99@gmail.com>
2020-10-04 20:06:20 +0100

    Added Easter Egg

---
+++ Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class EasterEgg : MonoBehaviour
+{
+    public void Trigger() {
+        Destroy(gameObject);
+        UIManager.instance.GetComponent<Animator>().Play("Achievement");
+    }
+}
